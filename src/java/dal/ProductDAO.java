38fdf4adadb8699d735ac479b33476d0f4f030f5
Dinhnamub2003 <115776189+Dinhnamub2003@users.noreply.github.com>
2024-03-05 22:05:22 +0700

    Fix

--- src/java/dal/ProductDAO.java
+++ src/java/dal/ProductDAO.java
@@ -18,28 +18,7 @@ import java.util.logging.Level;
  * @author Admin
  */
 public class ProductDAO extends DBContext {
-// Before
-//    public List<Product> getAllProduct() {
-//        List<Product> list = new ArrayList<>();
-//        String sql = "select * from Product";
-//        try {
-//            PreparedStatement ps = con.prepareStatement(sql);
-//            ResultSet rs = ps.executeQuery();
-//            while (rs.next()) {
-//                list.add(new Product(rs.getInt(1),
-//                        rs.getString(2),
-//                        rs.getString(3),
-//                        rs.getDouble(4),
-//                        rs.getString(5),
-//                        rs.getString(6)));
-//            }
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//        return list;
-//    }
-    
-//     After
+
      public List<Product> getAllProduct() {
         List<Product> list = new ArrayList<>();
         String sql = "select * from Product";
@@ -274,13 +253,7 @@ public class ProductDAO extends DBContext {
         }
         return list;
     }
-//    public static void main(String[] args) {
-//        ProductDAO pdb = new ProductDAO();
-//        List<Product> p = pdb.pagingProduct(1);
-//        for (Product product : p) {
-//            System.out.println(product.getName());
-//        }
-//    }
+
 
     public List<Product> getAllAndIndex(int id, int indexPage) {
         List<Product> list = new ArrayList<>();
